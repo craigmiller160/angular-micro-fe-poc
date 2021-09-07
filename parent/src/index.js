8c5e8231db493a659c7d8d3eaad8adc6a37230b2
Craig <craigmiller160@gmail.com>
2021-09-07 17:01:29 -0400

    Cleaned up parent config

--- parent/src/index.js
+++ parent/src/index.js
@@ -1,9 +1,3 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './index.scss';
-import('reactChild1');
-import('svelteChild1');
-import('vueChild1');
-import('reactChild2');
-import('svelteChild2');
-import('vueChild2');
